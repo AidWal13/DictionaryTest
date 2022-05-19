0d9e0e5b68121428691efe4490aaa7ff55c1935f
AidWal13 <aidanwalsh13@gmail.com>
2022-05-18 22:16:28 -0400

    Generate ingr labels

--- index.js
+++ index.js
@@ -5,25 +5,35 @@ let tradegoodChosen;
 const UserInv = new UserInventory();
 const NeededInv = new ShoppingListInventory();
 
-//Add event listeners to the options buttons so that when they are clicked, tradegoodchosen is assigned
+//get all buttons and add event listeners
 let ingrButtons = document.querySelectorAll(".option");
-
 ingrButtons.forEach((optionBtn) => {
     optionBtn.addEventListener('click', moveOnTGSetTo);
 });
 
 
+console.log(TradeGoods['thermiccondensate']);
 
 
-
-
-
-
-
+//Functions
 
 function moveOnTGSetTo(evt) {
-    console.log(`Within the event listener for TG: ${evt.currentTarget.id}.`)
-    tradegoodChosen = evt.currentTarget.id;
-    NeededInv.addGoodToInv(TradeGoods[tradegoodChosen]);
-    console.log(NeededInv);
+  evt.currentTarget.parentElement.style.display ='none';
+  evt.currentTarget.parentElement.nextElementSibling.style.display ='block';
+
+  tradegoodChosen = evt.currentTarget.id;
+  NeededInv.addGoodToInv(TradeGoods[tradegoodChosen]);
+  
+  TradeGoods[tradegoodChosen].ingredients.forEach((ingr) => {
+    makeIngrInput(ingr);
+  });
 }
+
+function makeIngrInput(ingredient) {
+    let labelEl = document.createElement('label');
+    labelEl.innerHTML = ingredient;
+    labelEl.setAttribute('for', ingredient.replace(/\s/g, ''));
+    let div = document.getElementById("divOfInputs");
+    div.appendChild(labelEl);
+}
+
