de57f76b8933d7f99c576985c1496bc28ff39a76
AidWal13 <aidanwalsh13@gmail.com>
2022-05-19 22:44:44 -0400

    add trade goods

--- TradeGoods.js
+++ TradeGoods.js
@@ -1,34 +1,10 @@
-//THERMIC CONDENSATE OBJECT
-export const thermiccondensate = {
-  name: 'thermiccondensate',
-  ingredients: ['sulphurine', 'condensed carbon'],
-  _sulphurine: 250,
-  '_condensed carbon': 50,
-  
-  get sulphurine() {
-    return this._sulphurine;
-  },
-
-  get 'condensed carbon'() {
-    return this['_condensed carbon'];
-  },
-    
-  set sulphurine(amt) {
-    console.log("WARNING: NOT ALLOWED TO EDIT THE AMOUNT REQUIRED FOR A RECIPE");
-  }, 
-  
-  set 'condensed carbon'(amt) {
-    console.log("WARNING: NOT ALLOWED TO EDIT THE AMOUNT REQUIRED FOR A RECIPE");
-  },
-};
-
-//NITROGEN SALT OBJECT
 export const nitrogensalt = {
   name: 'nitrogensalt',
+  baseLevel: true,
   ingredients: ['nitrogen', 'condensed carbon'],
   _nitrogen: 250,
   '_condensed carbon': 50,
-  
+
   get nitrogen() {
     return this._nitrogen;
   },
@@ -36,12 +12,161 @@ export const nitrogensalt = {
   get 'condensed carbon'() {
     return this['_condensed carbon'];
   },
-    
+
   set nitrogen(amt) {
     console.log("WARNING: NOT ALLOWED TO EDIT THE AMOUNT REQUIRED FOR A RECIPE");
   }, 
-  
+
   set 'condensed carbon'(amt) {
     console.log("WARNING: NOT ALLOWED TO EDIT THE AMOUNT REQUIRED FOR A RECIPE");
-  },
+  }
 };
+
+
+export const thermiccondensate = {
+  name: 'thermiccondensate',
+  baseLevel: true,
+  ingredients: ['sulphurine', 'condensed carbon'],
+  _sulphurine: 250,
+  '_condensed carbon': 50,
+
+  get sulphurine() {
+    return this._sulphurine;
+  },
+
+  get 'condensed carbon'() {
+    return this['_condensed carbon'];
+  },
+
+  set sulphurine(amt) {
+    console.log("WARNING: NOT ALLOWED TO EDIT THE AMOUNT REQUIRED FOR A RECIPE");
+  }, 
+
+  set 'condensed carbon'(amt) {
+    console.log("WARNING: NOT ALLOWED TO EDIT THE AMOUNT REQUIRED FOR A RECIPE");
+  }
+}
+
+export const enrichedcarbon = {
+  name: 'enrichedcarbon',
+  baseLevel: true,
+  ingredients: ['radon', 'condensed carbon'],
+  _radon: 250,
+  '_condensed carbon': 50,
+
+  get radon() {
+    return this._radon;
+  },
+
+  get 'condensed carbon'() {
+    return this['_condensed carbon'];
+  },
+
+  set radon(amt) {
+    console.log("WARNING: NOT ALLOWED TO EDIT THE AMOUNT REQUIRED FOR A RECIPE");
+  }, 
+
+  set 'condensed carbon'(amt) {
+    console.log("WARNING: NOT ALLOWED TO EDIT THE AMOUNT REQUIRED FOR A RECIPE");
+  }
+}
+
+export const aronium = {
+  name: 'aronium',
+  baseLevel: true,
+  ingredients: ['paraffinium', 'ionised cobalt'],
+  _paraffinium: 50,
+  '_ionised cobalt': 50,
+
+  get paraffinium() {
+    return this._paraffinium;
+  },
+
+  get 'ionised cobalt'() {
+    return this['_ionised cobalt'];
+  },
+
+  set paraffinium(amt) {
+    console.log("WARNING: NOT ALLOWED TO EDIT THE AMOUNT REQUIRED FOR A RECIPE");
+  }, 
+
+  set 'ionised cobalt'(amt) {
+    console.log("WARNING: NOT ALLOWED TO EDIT THE AMOUNT REQUIRED FOR A RECIPE");
+  }
+}
+
+export const magnogold = {
+  name: 'magno-gold',
+  baseLevel: true,
+  ingredients: ['phosphorus', 'ionised cobalt'],
+  _phosphorus: 50,
+  '_ionised cobalt': 50,
+
+  get phosphorus() {
+    return this._phosphorus;
+  },
+
+  get 'ionised cobalt'() {
+    return this['_ionised cobalt'];
+  },
+
+  set phosphorus(amt) {
+    console.log("WARNING: NOT ALLOWED TO EDIT THE AMOUNT REQUIRED FOR A RECIPE");
+  }, 
+
+  set 'ionised cobalt'(amt) {
+    console.log("WARNING: NOT ALLOWED TO EDIT THE AMOUNT REQUIRED FOR A RECIPE");
+  }
+}
+
+export const grantine = {
+  name: 'grantine',
+  baseLevel: true,
+  ingredients: ['dioxite', 'ionised cobalt'],
+  _dioxite: 50,
+  '_ionised cobalt': 50,
+
+  get dioxite() {
+    return this._dioxite;
+  },
+
+  get 'ionised cobalt'() {
+    return this['_ionised cobalt'];
+  },
+
+  set dioxite(amt) {
+    console.log("WARNING: NOT ALLOWED TO EDIT THE AMOUNT REQUIRED FOR A RECIPE");
+  }, 
+
+  set 'ionised cobalt'(amt) {
+    console.log("WARNING: NOT ALLOWED TO EDIT THE AMOUNT REQUIRED FOR A RECIPE");
+  }
+}
+
+
+
+
+
+
+export const geodesite = {
+  name: 'geodesite',
+  baseLevel: false,
+  ingredients: ['dirty bronze', 'herox', 'lemmium'],
+  '_dirty bronze': 1,
+  _herox: 1,
+  _lemmium: 1,
+
+  
+
+  set herox(amt) {
+    console.log("WARNING: NOT ALLOWED TO EDIT THE AMOUNT REQUIRED FOR A RECIPE");
+  }, 
+
+  set 'dirty bronze'(amt) {
+    console.log("WARNING: NOT ALLOWED TO EDIT THE AMOUNT REQUIRED FOR A RECIPE");
+  },
+
+  set lemmium(amt) {
+    console.log("WARNING: NOT ALLOWED TO EDIT THE AMOUNT REQUIRED FOR A RECIPE");
+  }
+}
--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 import { Inventory , UserInventory , ShoppingListInventory } from '/Inventory.js';
 import * as TradeGoods from '/TradeGoods.js';
 
+//ACTUAL CODING -------------------------------------------------------------------------
+
 let tradegoodChosen;
 const UserInv = new UserInventory();
 const NeededInv = new ShoppingListInventory();
+let collectInputs = UserInv.collectInputs.bind(UserInv);
 
 //get all buttons and add event listeners
 let ingrButtons = document.querySelectorAll(".option");
+
+
 ingrButtons.forEach((optionBtn) => {
+    console.log(`Event Listener Added to: ${optionBtn}.`);
     optionBtn.addEventListener('click', moveOnTGSetTo);
 });
-
-
-console.log(TradeGoods['thermiccondensate']);
-
 
 //Functions
 
@@ -27,13 +29,39 @@ function moveOnTGSetTo(evt) {
   TradeGoods[tradegoodChosen].ingredients.forEach((ingr) => {
     makeIngrInput(ingr);
   });
+  
+  addSubmitButton(evt.currentTarget.parentElement.nextElementSibling);
 }
 
 function makeIngrInput(ingredient) {
-    let labelEl = document.createElement('label');
-    labelEl.innerHTML = ingredient;
-    labelEl.setAttribute('for', ingredient.replace(/\s/g, ''));
-    let div = document.getElementById("divOfInputs");
-    div.appendChild(labelEl);
+  //Create the label element
+  let labelEl = document.createElement('label');
+  labelEl.innerHTML = ingredient;
+  labelEl.setAttribute('for', ingredient.replace(/\s/g, ''));
+    
+  //Create the input element
+  let inputEl = document.createElement('input');
+  inputEl.id = ingredient;
+  inputEl.type = "number";
+  inputEl.name = ingredient.replace(/\s/g, '');
+  inputEl.classList.add('ingrInput');
+  inputEl.placeholder = TradeGoods[tradegoodChosen][ingredient];
+    
+  
+  let div = document.getElementById("divOfInputs");
+  div.appendChild(labelEl);
+  div.appendChild(inputEl);
+  div.appendChild(document.createElement("br"));
 }
 
+function addSubmitButton(el) {
+  let submitButton = document.createElement('input');
+  submitButton.type = "submit";
+  submitButton.id = "ingredientSubmit";
+  submitButton.onclick = collectInputs;
+  
+  
+  //let div = document.getElementById("divOfInputs");
+  el.appendChild(document.createElement("br"));
+  el.appendChild(submitButton);
+}
